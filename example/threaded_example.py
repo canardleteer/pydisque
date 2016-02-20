0b83f73777a0931eb17c75bbf0f42bc5f99cc12e
dylan d <eris.has.a.dad@gmail.com>
2016-02-20 10:04:35 -0800

    A few more additions to the threading example.

--- example/threaded_example.py
+++ example/threaded_example.py
@@ -2,9 +2,18 @@
 Experimenting with threads and pydisque.
 
 It could be entirely possible that pydisque or a subset of
-it's sublibraries are not thread safe, this should help
-test that.
+it's sublibraries are not thread safe, but so far things
+appear to be solid.
+
+Leaving the queue PAUSE stuff out for now, we really don't
+support it in the main lib properly.
+
+TODO:
+    DisqueThread superclass
+    Really need to detect PAUSED queues better then ResponseError
+    Manage threads and exiting better
 """
+import sys
 import json
 import time
 import logging
@@ -13,6 +22,9 @@ import threading
 # logging.basicConfig(level=logging.DEBUG)
 
 from pydisque.client import Client
+
+# TODO: get rid of this requirement in the main lib
+from redis.exceptions import ResponseError
 
 PRODUCER_THREADS=4
 CONSUMER_THREADS=4
@@ -27,8 +39,13 @@ class consumerThread(threading.Thread):
 
     def run(self):
         while self._running:
-            jerb = self._client.get_job([SHARED_QUEUE])
-            print(jerb)
+            try:
+                jerb = self._client.get_job([SHARED_QUEUE])
+                print(jerb)
+            except ResponseError:
+                print(
+                    "consumer #%d: Queue paused, skipping." % self._identifier)
+            time.sleep(0.1)
 
 class producerThread(threading.Thread):
     def __init__(self, client, identifier):
@@ -39,9 +56,29 @@ class producerThread(threading.Thread):
 
     def run(self):
         while(self._running):
-            self._client.add_job(SHARED_QUEUE,
-                                "%d-%d" % (self._identifier, time.time()),
-                                replicate=1, timeout=100)
+            try:
+                self._client.add_job(SHARED_QUEUE,
+                                    "%d-%d" % (self._identifier, time.time()),
+                                    replicate=1, timeout=100)
+            except ResponseError:
+                print(
+                    "producer #%d: Queue paused, skipping." % self._identifier)
+            time.sleep(0.1)
+
+class pausingThread(threading.Thread):
+    def __init__(self, client, identifier):
+        self._client = client
+        self._identifier = identifier
+        self._running = True
+        threading.Thread.__init__(self)
+
+    def run(self):
+        while(self._running):
+            self._client.pause(SHARED_QUEUE, kw_all=True)
+            napTimer = random.randint(0,10)
+            print("Pausing for: %ds" % napTimer)
+            time.sleep(napTimer)
+            self._client.pause(SHARED_QUEUE, kw_none=True)
 
 def main():
 
@@ -56,11 +93,14 @@ def main():
         ct = consumerThread(client, i)
         ct.start()
 
+    # pt = pausingThread(client, "0")
+    # pt.start()
+
     try:
-        while True:
-            time.sleep(1)
+        while threading.activeCount() > 0:
+            time.sleep(0.1)
     except KeyboardInterrupt:
-        sys.quit()
+        sys.exit()
 
 if __name__ == "__main__":
         main()
